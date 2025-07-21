6e3d4260823f1a19f2e5bc76a716b8ca8d4f212b
Keerthi RH <162913059+Keerthi-2025@users.noreply.github.com>
2025-07-21 22:27:14 +0530

    Update Home.jsx

--- frontend1/src/pages/Home.jsx
+++ frontend1/src/pages/Home.jsx
@@ -2,12 +2,6 @@ import React from 'react'
 import HeroSection from '../components/Herosection';
 
 
-const Home = () => {
-  return (
-    <>
-    <HeroSection/>
-    </>
-  )
-}
+
 
 export default Home;
